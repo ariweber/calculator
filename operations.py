a1cca297e69255476cef417dd67d31c769ce051d
aharon weber <a.w770770@gmail.com>
2026-02-02 15:11:52 +0200

    addwd power

--- operations.py
+++ operations.py
@@ -7,8 +7,9 @@ def sub(num1,num2):
 def multiply(num1,num2):
     return num1 * num2
 
-def divide(num1,num2):
+def divide(num1,num2)6
+:
       if num2 == 0:
          return "Error: Cannot divide by zero!"
 def power(num1, num2):
-    return num1 ** num2
+    return num1 ** nu
