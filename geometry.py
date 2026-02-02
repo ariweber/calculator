2e3cd76bdc4a3fe867452ea167be2550e22c5fa3
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-02 14:28:09 +0200

    Geometric calculations

---
+++ geometry.py
@@ -0,0 +1,11 @@
+import math
+
+def area_circle(radius):
+    return math.pi * (radius ** 2)
+
+def area_rectangle(width, height):
+     return width * height
+
+def perimeter_rectangle(width, height):
+    return 2 * (width + height)
+
