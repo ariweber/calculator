3a83d36c88855d0a861c0f5e210a633bbd9cdb2d
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-02 12:04:27 +0200

    Correcting function names

--- operations.py
+++ operations.py
@@ -1,13 +1,13 @@
-def add_numbers(num1,num2):
+def add(num1,num2):
     return num1 + num2
 
-def sub_numbers(num1,num2):
+def sub(num1,num2):
     return num1 - num2
 
-def mul_numbers(num1,num2):
+def multiply(num1,num2):
     return num1 * num2
 
-def divide_numbers(num1,num2):
+def divide(num1,num2):
       if num2 == 0:
          return "Error: Cannot divide by zero!"
       return num1 / num2
